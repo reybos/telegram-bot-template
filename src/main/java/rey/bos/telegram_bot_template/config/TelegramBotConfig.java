a0f206f205895c07f102581be191d98282990d95
reybos <andreybosiy@yandex.ru>
2024-05-03 12:08:49 +0200

    telegram bot template

---
+++ src/main/java/rey/bos/telegram_bot_template/config/TelegramBotConfig.java
@@ -0,0 +1,16 @@
+package rey.bos.telegram_bot_template.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "telegram")
+@Getter
+@Setter
+public class TelegramBotConfig {
+
+    private String token;
+
+}
